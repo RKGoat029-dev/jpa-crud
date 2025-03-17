9db746b7c6688ff1fdfefe6a283fd7b89bc34e0a
RKGoat029-dev <rkgoat029.dev@gmail.com>
2025-03-17 13:31:18 +0100

    Added DAO Methods

--- demo/src/main/java/com/example/demo/DAO/CarDAO.java
+++ demo/src/main/java/com/example/demo/DAO/CarDAO.java
@@ -1,11 +1,18 @@
 package com.example.demo.DAO;
 
 import com.example.demo.entity.Car;
+import java.util.List;
 
 public interface CarDAO {
 
     void save(Car car);
 
     Car findById(int id);
+
+    List<Car> findAll();
+
+    List<Car> findByManufacter(String manufacter);
+
+    List<Car> findByModel(String model);
 
 }
--- demo/src/main/java/com/example/demo/DAO/CarDAOImpl.java
+++ demo/src/main/java/com/example/demo/DAO/CarDAOImpl.java
@@ -2,9 +2,13 @@ package com.example.demo.DAO;
 
 import com.example.demo.entity.Car;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 @Repository
 public class CarDAOImpl implements CarDAO {
@@ -20,4 +24,44 @@ public class CarDAOImpl implements CarDAO {
     @Override
     public Car findById(int id) { return this.entityManager.find(Car.class, id); }
 
+    @Override
+    public List<Car> findAll() {
+
+        TypedQuery<Car> allQuery;
+
+        allQuery = entityManager.createQuery("FROM Car", Car.class);
+
+        return allQuery.getResultList();
+
+    }
+
+    @Override
+    public List<Car> findByManufacter(String manufacter) {
+
+        TypedQuery<Car> manufacterQuery;
+
+        manufacterQuery = entityManager.createQuery(
+                "FROM Car WHERE manufacter=:manufacter", Car.class
+        );
+
+        manufacterQuery.setParameter("manufacter", manufacter);
+
+        return manufacterQuery.getResultList();
+
+    }
+
+    @Override
+    public List<Car> findByModel(String model) {
+
+        TypedQuery<Car> modelQuery;
+
+        modelQuery = entityManager.createQuery(
+                "FROM Car WHERE model=:model", Car.class
+        );
+
+        modelQuery.setParameter("model", model);
+
+        return modelQuery.getResultList();
+
+    }
 }
